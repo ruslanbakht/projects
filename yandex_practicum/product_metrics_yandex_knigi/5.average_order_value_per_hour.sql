bb5430b8fb28e6089df1da3b372f6ad495cfddd4
Руслан Бахтигареев <ruslanbakht@gmail.com>
2025-08-15 15:12:49 +0500

    Update 5.average_order_value_per_hour.sql

--- yandex_practicum/product_metrics_yandex_knigi/5.average_order_value_per_hour.sql
+++ yandex_practicum/product_metrics_yandex_knigi/5.average_order_value_per_hour.sql
@@ -13,3 +13,9 @@ SELECT month,
        ROUND(mau*399.0/hours,2)  AS avg_hour_rev
 FROM cte
 GROUP BY month, mau, hours  
+
+Результат
+month	mau	hours	avg_hour_rev
+2024-09-01	16320	105539	61.7
+2024-10-01	18280	137384	53.09
+2024-11-01	18594	145351	51.04
