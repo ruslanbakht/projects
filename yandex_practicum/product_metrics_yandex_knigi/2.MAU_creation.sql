df7cf396d63b7e7645beb9ddb9005047bd310bdc
Руслан Бахтигареев <ruslanbakht@gmail.com>
2025-08-15 15:09:58 +0500

    Update 2.MAU_creation.sql

--- yandex_practicum/product_metrics_yandex_knigi/2.MAU_creation.sql
+++ yandex_practicum/product_metrics_yandex_knigi/2.MAU_creation.sql
@@ -11,3 +11,10 @@ WHERE EXTRACT(MONTH FROM msk_business_dt_str)  = 11
 GROUP BY main_content_name, published_topic_title_list, main_author_name
 ORDER BY mau DESC
 LIMIT 3;
+
+Результат
+main_content_name	published_topic_title_list	main_author_name	mau
+Собачка Соня на даче	['Детская проза и поэзия', 'Аудио']	Андрей Усачев	4597
+Женькин клад и другие школьные рассказы	['Сказки и фольклор', 'Детская проза и поэзия', 'Аудио']	Игорь Носов	3050
+Знаменитая собачка Соня	['Аудиоспектакли', 'Детская проза и поэзия', 'Аудио']	Андрей Усачев	2785
+
